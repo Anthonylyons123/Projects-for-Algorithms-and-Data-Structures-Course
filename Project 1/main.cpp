07f1777e40ebab83679d41ae48866161041e5b9a
Anthonylyons123 <lyons.anthony20@gmail.com>
2025-01-23 22:48:01 -0500

    Finished the Comments for real

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -10,33 +10,33 @@ int main(){
     std::string command;
     while (true){
         std::cin >> command;
-        if (command == "LOAD"){
+        if (command == "LOAD"){ //Calls LOAD function
             std::cin >> command; 
             temp.LOAD(command);
             std::cout << "success" <<std::endl;
-        } else if (command == "PRINT"){
+        } else if (command == "PRINT"){ //Calls Print Function
             temp.PRINT();
-        } else if (command == "ADD"){
+        } else if (command == "ADD"){ //Calls Add function and gets year and data inputs from user
             int year{0};
             double data{0};
             std::cin >> year;
             std::cin >> data;
             temp.ADD(data, year);
-        } else if (command == "UPDATE"){
+        } else if (command == "UPDATE"){ //Calls Update function and gets year and data inputs from user
             int year{0};
             double data{0};
             std::cin >> year;
             std::cin >> data;
             temp.UPDATE(data, year);
-        } else if (command == "MEAN"){
+        } else if (command == "MEAN"){ //Calls Mean function
             temp.mean();
-        } else if (command == "MONOTONIC"){
+        } else if (command == "MONOTONIC"){ //Calls monotonic fucntion
             temp.is_monotonic();
-        } else if (command == "FIT"){
+        } else if (command == "FIT"){ //Calls fit funtion
             double m{0};
             double b{0};
             temp.best_fit(m, b);
-        } else if (command == "EXIT"){
+        } else if (command == "EXIT"){ //Terminates the program if exit is called
             return 0;
         }
     }
